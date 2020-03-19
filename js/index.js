351b60c8903726944df68824bc11e69d17122ace
Alex Feoktistov <Feoktistovao@gmail.com>
2020-03-19 20:27:37 +0300

    Task.4 PrintTriangle Js
    PrintTriangle in console

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function printTriangle(rows)
+{
+    debugger
+    for (let i = 0; i < rows; i++) {
+        var output = '';
+        for (let j =0; j < rows - i; j++) output += ' ';
+        for (let k = 0; k <= i; k++) output += '# ';
+        console.log(output);
+    }
+}
